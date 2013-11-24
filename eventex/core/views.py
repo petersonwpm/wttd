cc88be47848afabef657afaf281add01f16a867a
Williams P Marinho <petersonwpm@gmail.com>
2013-11-24 21:45:34 -0200

    alteracao da view para utilizar render_to_response

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
-from django.template import loader, Context
+from django.shortcuts import render_to_response
 
 def home(request):
-    t = loader.get_template('index.html')
-    c = Context()
-
-    content = t.render(c)
-
-    return HttpResponse(content)
+    return render_to_response('index.html')
